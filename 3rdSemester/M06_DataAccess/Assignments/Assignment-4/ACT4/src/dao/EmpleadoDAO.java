d24a1957ee1edd88749d96fd9392771ca2779492
Ignasi Moya <imoyadv@gmail.com>
2025-04-21 13:32:07 +0200

    Refactor EmpleadoDAO

--- 3rdSemester/M06_DataAccess/Assignments/Assignment-4/ACT4/src/dao/EmpleadoDAO.java
+++ 3rdSemester/M06_DataAccess/Assignments/Assignment-4/ACT4/src/dao/EmpleadoDAO.java
@@ -25,64 +25,33 @@ public class EmpleadoDAO {
     private static final String DOCUMENTO = "empleados.xml";
 
     public void insertarEmpleado(Empleado empleado) throws ExcepcionAccesoBaseDeDatos {
-        Collection collection = null;
+        String xquery = String.format("""
+            update insert
+                <empleado id="%d">
+                    <usuario>%s</usuario>
+                    <contrasena>%s</contrasena>
+                    <nombre_completo>%s</nombre_completo>
+                    <telefono>%s</telefono>
+                    <correo_electronico>%s</correo_electronico>
+                    <puesto>%s</puesto>
+                </empleado>
+            into doc("%s")/empleados
+            """,
+                empleado.getId(),
+                empleado.getUsuario(),
+                empleado.getContrasena(),
+                empleado.getNombreCompleto(),
+                empleado.getTelefono(),
+                empleado.getCorreoElectronico(),
+                empleado.getPuesto(),
+                DOCUMENTO
+        );
 
-        try {
-            collection = ConexionExistDB.getInstancia().obtenerColeccion(RUTA_COLECCION);
-
-            XQueryService service = (XQueryService) collection.getService("XQueryService", "1.0");
-            service.setProperty(OutputKeys.INDENT, "yes");
-            service.setProperty(OutputKeys.ENCODING, "UTF-8");
-
-            String xquery = String.format("""
-                update insert
-                    <empleado id="%d">
-                        <usuario>%s</usuario>
-                        <contrasena>%s</contrasena>
-                        <nombre_completo>%s</nombre_completo>
-                        <telefono>%s</telefono>
-                        <correo_electronico>%s</correo_electronico>
-                        <puesto>%s</puesto>
-                    </empleado>
-                into doc("%s")/empleados
-                """,
-                    empleado.getId(),
-                    empleado.getUsuario(),
-                    empleado.getContrasena(),
-                    empleado.getNombreCompleto(),
-                    empleado.getTelefono(),
-                    empleado.getCorreoElectronico(),
-                    empleado.getPuesto(),
-                    DOCUMENTO
-            );
-
-            service.execute(service.compile(xquery));
-
-            System.out.println("Empleado insertado correctamente en empleados.xml");
-
-        } catch (XMLDBException e) {
-            throw new ExcepcionAccesoBaseDeDatos("Error al insertar empleado: " + e.getMessage(), e);
-        } finally {
-            try {
-                if (collection != null) collection.close();
-            } catch (XMLDBException e) {
-                e.printStackTrace();
-            }
-        }
+        ejecutarXQuerySinResultado(xquery, "Error al insertar empleado");
     }
 
     public Empleado validarEmpleado(String usuario, String contrasena) throws ExcepcionAccesoBaseDeDatos {
-        Collection collection = null;
-        Empleado empleado = null;
-
-        try {
-            collection = ConexionExistDB.getInstancia().obtenerColeccion(RUTA_COLECCION);
-
-            XQueryService service = (XQueryService) collection.getService("XQueryService", "1.0");
-            service.setProperty(OutputKeys.INDENT, "yes");
-            service.setProperty(OutputKeys.ENCODING, "UTF-8");
-
-            String xquery = String.format("""
+        String xquery = String.format("""
             for $e in doc("%s")/empleados/empleado
             where $e/usuario = '%s' and $e/contrasena = '%s'
             return
@@ -96,60 +65,15 @@ public class EmpleadoDAO {
                 </empleado>
             """, DOCUMENTO, usuario, contrasena);
 
-            var result = service.query(xquery);
-            var iterator = result.getIterator();
-
-            if (iterator.hasMoreResources()) {
-                var res = iterator.nextResource();
-                String contenido = res.getContent().toString();
-
-                // Muy básico: lo ideal sería parsear con DOM o JAXB, pero lo hacemos por extracción directa
-                empleado = new Empleado();
-                empleado.setUsuario(usuario);
-                empleado.setContrasena(contrasena);
-
-                if (contenido.contains("id=\"")) {
-                    int id = Integer.parseInt(contenido.replaceAll(".*id=\"(\\d+)\".*", "$1"));
-                    empleado.setId(id);
-                }
-                if (contenido.contains("<nombre_completo>")) {
-                    empleado.setNombreCompleto(contenido.replaceAll(".*<nombre_completo>(.+)</nombre_completo>.*", "$1"));
-                }
-                if (contenido.contains("<telefono>")) {
-                    empleado.setTelefono(contenido.replaceAll(".*<telefono>(.+)</telefono>.*", "$1"));
-                }
-                if (contenido.contains("<correo_electronico>")) {
-                    empleado.setCorreoElectronico(contenido.replaceAll(".*<correo_electronico>(.+)</correo_electronico>.*", "$1"));
-                }
-                if (contenido.contains("<puesto>")) {
-                    empleado.setPuesto(contenido.replaceAll(".*<puesto>(.+)</puesto>.*", "$1"));
-                }
-            }
-
-        } catch (XMLDBException e) {
-            throw new ExcepcionAccesoBaseDeDatos("Error al validar empleado: " + e.getMessage(), e);
-        } finally {
-            try {
-                if (collection != null) collection.close();
-            } catch (XMLDBException e) {
-                e.printStackTrace();
-            }
+        try {
+            return ejecutarConsultaEmpleadoUnico(xquery);
+        } catch (Exception e) {
+            throw new ExcepcionAccesoBaseDeDatos("Error al validar empleado", e);
         }
-
-        return empleado;
     }
 
     public void modificarEmpleado(Empleado empleado) throws ExcepcionAccesoBaseDeDatos {
-        Collection collection = null;
-
-        try {
-            collection = ConexionExistDB.getInstancia().obtenerColeccion(RUTA_COLECCION);
-
-            XQueryService service = (XQueryService) collection.getService("XQueryService", "1.0");
-            service.setProperty(OutputKeys.INDENT, "yes");
-            service.setProperty(OutputKeys.ENCODING, "UTF-8");
-
-            String xquery = String.format("""
+        String xquery = String.format("""
             let $empleado := doc("%s")/empleados/empleado[@id = %d]
             return (
                 update replace $empleado/nombre_completo with <nombre_completo>%s</nombre_completo>,
@@ -158,40 +82,19 @@ public class EmpleadoDAO {
                 update replace $empleado/puesto with <puesto>%s</puesto>
             )
             """,
-                    DOCUMENTO,
-                    empleado.getId(),
-                    empleado.getNombreCompleto(),
-                    empleado.getTelefono(),
-                    empleado.getCorreoElectronico(),
-                    empleado.getPuesto()
-            );
+                DOCUMENTO,
+                empleado.getId(),
+                empleado.getNombreCompleto(),
+                empleado.getTelefono(),
+                empleado.getCorreoElectronico(),
+                empleado.getPuesto()
+        );
 
-            service.execute(service.compile(xquery));
-            System.out.println("Empleado modificado correctamente.");
-
-        } catch (XMLDBException e) {
-            throw new ExcepcionAccesoBaseDeDatos("Error al modificar empleado: " + e.getMessage(), e);
-        } finally {
-            try {
-                if (collection != null) collection.close();
-            } catch (XMLDBException e) {
-                e.printStackTrace();
-            }
-        }
+        ejecutarXQuerySinResultado(xquery, "Error al modificar empleado");
     }
 
     public Empleado buscarEmpleado(int id) throws ExcepcionAccesoBaseDeDatos {
-        Collection collection = null;
-        Empleado empleado = null;
-
-        try {
-            collection = ConexionExistDB.getInstancia().obtenerColeccion(RUTA_COLECCION);
-
-            XQueryService service = (XQueryService) collection.getService("XQueryService", "1.0");
-            service.setProperty(OutputKeys.INDENT, "yes");
-            service.setProperty(OutputKeys.ENCODING, "UTF-8");
-
-            String xquery = String.format("""
+        String xquery = String.format("""
             for $e in doc("%s")/empleados/empleado[@id = %d]
             return
                 <empleado id="{$e/@id}">
@@ -204,118 +107,32 @@ public class EmpleadoDAO {
                 </empleado>
             """, DOCUMENTO, id);
 
-            var result = service.query(xquery);
-            var iterator = result.getIterator();
-
-            if (iterator.hasMoreResources()) {
-                var res = iterator.nextResource();
-                String contenido = res.getContent().toString();
-
-                empleado = new Empleado();
-                empleado.setId(id);
-
-                if (contenido.contains("<usuario>")) {
-                    empleado.setUsuario(contenido.replaceAll(".*<usuario>(.+)</usuario>.*", "$1"));
-                }
-                if (contenido.contains("<contrasena>")) {
-                    empleado.setContrasena(contenido.replaceAll(".*<contrasena>(.+)</contrasena>.*", "$1"));
-                }
-                if (contenido.contains("<nombre_completo>")) {
-                    empleado.setNombreCompleto(contenido.replaceAll(".*<nombre_completo>(.+)</nombre_completo>.*", "$1"));
-                }
-                if (contenido.contains("<telefono>")) {
-                    empleado.setTelefono(contenido.replaceAll(".*<telefono>(.+)</telefono>.*", "$1"));
-                }
-                if (contenido.contains("<correo_electronico>")) {
-                    empleado.setCorreoElectronico(contenido.replaceAll(".*<correo_electronico>(.+)</correo_electronico>.*", "$1"));
-                }
-                if (contenido.contains("<puesto>")) {
-                    empleado.setPuesto(contenido.replaceAll(".*<puesto>(.+)</puesto>.*", "$1"));
-                }
-            }
-
-        } catch (XMLDBException e) {
-            throw new ExcepcionAccesoBaseDeDatos("Error al buscar empleado por ID: " + e.getMessage(), e);
-        } finally {
-            try {
-                if (collection != null) collection.close();
-            } catch (XMLDBException e) {
-                e.printStackTrace();
-            }
+        try {
+            return ejecutarConsultaEmpleadoUnico(xquery);
+        } catch (Exception e) {
+            throw new ExcepcionAccesoBaseDeDatos("Error al buscar empleado", e);
         }
-
-        return empleado;
     }
 
     public void cambiarContrasena(int id, String nuevaContrasena) throws ExcepcionAccesoBaseDeDatos {
-        Collection collection = null;
-
-        try {
-            collection = ConexionExistDB.getInstancia().obtenerColeccion(RUTA_COLECCION);
-
-            XQueryService service = (XQueryService) collection.getService("XQueryService", "1.0");
-            service.setProperty(OutputKeys.INDENT, "yes");
-            service.setProperty(OutputKeys.ENCODING, "UTF-8");
-
-            String xquery = String.format("""
+        String xquery = String.format("""
             let $empleado := doc("%s")/empleados/empleado[@id = %d]
             return update replace $empleado/contrasena with <contrasena>%s</contrasena>
             """, DOCUMENTO, id, nuevaContrasena);
 
-            service.execute(service.compile(xquery));
-            System.out.println("Contraseña actualizada correctamente para el empleado con ID " + id);
-
-        } catch (XMLDBException e) {
-            throw new ExcepcionAccesoBaseDeDatos("Error al cambiar la contraseña del empleado: " + e.getMessage(), e);
-        } finally {
-            try {
-                if (collection != null) collection.close();
-            } catch (XMLDBException e) {
-                e.printStackTrace();
-            }
-        }
+        ejecutarXQuerySinResultado(xquery, "Error al cambiar la contraseña del empleado");
     }
 
     public void eliminarEmpleado(int id) throws ExcepcionAccesoBaseDeDatos {
-        Collection collection = null;
-
-        try {
-            collection = ConexionExistDB.getInstancia().obtenerColeccion(RUTA_COLECCION);
-
-            XQueryService service = (XQueryService) collection.getService("XQueryService", "1.0");
-            service.setProperty(OutputKeys.INDENT, "yes");
-            service.setProperty(OutputKeys.ENCODING, "UTF-8");
-
-            String xquery = String.format("""
+        String xquery = String.format("""
             update delete doc("%s")/empleados/empleado[@id = %d]
             """, DOCUMENTO, id);
 
-            service.execute(service.compile(xquery));
-            System.out.println("Empleado eliminado correctamente.");
-
-        } catch (XMLDBException e) {
-            throw new ExcepcionAccesoBaseDeDatos("Error al eliminar empleado: " + e.getMessage(), e);
-        } finally {
-            try {
-                if (collection != null) collection.close();
-            } catch (XMLDBException e) {
-                e.printStackTrace();
-            }
-        }
+        ejecutarXQuerySinResultado(xquery, "Error al eliminar empleado");
     }
 
     public List<Empleado> obtenerEmpleados() throws ExcepcionAccesoBaseDeDatos {
-        Collection collection = null;
-        List<Empleado> empleados = new ArrayList<>();
-
-        try {
-            collection = ConexionExistDB.getInstancia().obtenerColeccion(RUTA_COLECCION);
-
-            XQueryService service = (XQueryService) collection.getService("XQueryService", "1.0");
-            service.setProperty(OutputKeys.INDENT, "yes");
-            service.setProperty(OutputKeys.ENCODING, "UTF-8");
-
-            String xquery = String.format("""
+        String xquery = String.format("""
             for $e in doc("%s")/empleados/empleado
             return
                 <empleado id="{$e/@id}">
@@ -328,43 +145,18 @@ public class EmpleadoDAO {
                 </empleado>
             """, DOCUMENTO);
 
-            var result = service.execute(service.compile(xquery));
-            var iterator = result.getIterator();
-
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-
-            while (iterator.hasMoreResources()) {
-                var res = iterator.nextResource();
-                String contenido = res.getContent().toString();
-
-                Document doc = builder.parse(new InputSource(new StringReader(contenido)));
-                Element root = doc.getDocumentElement();
-
-                Empleado empleado = new Empleado();
-
-                empleado.setId(Integer.parseInt(root.getAttribute("id")));
-                empleado.setUsuario(getText(root, "usuario"));
-                empleado.setContrasena(getText(root, "contrasena"));
-                empleado.setNombreCompleto(getText(root, "nombre_completo"));
-                empleado.setTelefono(getText(root, "telefono"));
-                empleado.setCorreoElectronico(getText(root, "correo_electronico"));
-                empleado.setPuesto(getText(root, "puesto"));
-
-                empleados.add(empleado);
-            }
-
+        try {
+            return ejecutarConsultaEmpleados(xquery);
         } catch (Exception e) {
-            throw new ExcepcionAccesoBaseDeDatos("Error al obtener los empleados: " + e.getMessage(), e);
-        } finally {
-            try {
-                if (collection != null) collection.close();
-            } catch (XMLDBException e) {
-                e.printStackTrace();
-            }
+            throw new ExcepcionAccesoBaseDeDatos("Error al obtener empleados", e);
         }
+    }
 
-        return empleados;
+    private XQueryService obtenerServicio(Collection collection) throws XMLDBException {
+        XQueryService service = (XQueryService) collection.getService("XQueryService", "1.0");
+        service.setProperty(OutputKeys.INDENT, "yes");
+        service.setProperty(OutputKeys.ENCODING, "UTF-8");
+        return service;
     }
 
     private String getText(Element padre, String etiqueta) {
@@ -373,5 +165,88 @@ public class EmpleadoDAO {
             return nodos.item(0).getTextContent();
         }
         return "";
+    }
+
+    private Empleado parsearEmpleadoDesdeXML(String contenido) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new InputSource(new StringReader(contenido)));
+        Element root = doc.getDocumentElement();
+
+        Empleado empleado = new Empleado();
+        empleado.setId(Integer.parseInt(root.getAttribute("id")));
+        empleado.setUsuario(getText(root, "usuario"));
+        empleado.setContrasena(getText(root, "contrasena"));
+        empleado.setNombreCompleto(getText(root, "nombre_completo"));
+        empleado.setTelefono(getText(root, "telefono"));
+        empleado.setCorreoElectronico(getText(root, "correo_electronico"));
+        empleado.setPuesto(getText(root, "puesto"));
+
+        return empleado;
+    }
+
+    private List<Empleado> ejecutarConsultaEmpleados(String xquery) throws Exception {
+        Collection collection = null;
+        List<Empleado> empleados = new ArrayList<>();
+
+        try {
+            collection = ConexionExistDB.getInstancia().obtenerColeccion(RUTA_COLECCION);
+            XQueryService service = obtenerServicio(collection);
+
+            var result = service.execute(service.compile(xquery));
+            var iterator = result.getIterator();
+
+            while (iterator.hasMoreResources()) {
+                var res = iterator.nextResource();
+                empleados.add(parsearEmpleadoDesdeXML(res.getContent().toString()));
+            }
+
+        } finally {
+            if (collection != null) collection.close();
+        }
+
+        return empleados;
+    }
+
+    private Empleado ejecutarConsultaEmpleadoUnico(String xquery) throws Exception {
+        Collection collection = null;
+
+        try {
+            collection = ConexionExistDB.getInstancia().obtenerColeccion(RUTA_COLECCION);
+            XQueryService service = obtenerServicio(collection);
+
+            var result = service.execute(service.compile(xquery));
+            var iterator = result.getIterator();
+
+            if (iterator.hasMoreResources()) {
+                var res = iterator.nextResource();
+                return parsearEmpleadoDesdeXML(res.getContent().toString());
+            }
+
+            return null;
+
+        } finally {
+            if (collection != null) collection.close();
+        }
+    }
+
+    private void ejecutarXQuerySinResultado(String xquery, String mensajeError) throws ExcepcionAccesoBaseDeDatos {
+        Collection collection = null;
+
+        try {
+            collection = ConexionExistDB.getInstancia().obtenerColeccion(RUTA_COLECCION);
+            XQueryService service = obtenerServicio(collection);
+            service.execute(service.compile(xquery));
+
+        } catch (Exception e) {
+            throw new ExcepcionAccesoBaseDeDatos(mensajeError + ": " + e.getMessage(), e);
+
+        } finally {
+            try {
+                if (collection != null) collection.close();
+            } catch (XMLDBException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
